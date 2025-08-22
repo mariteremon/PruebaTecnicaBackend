e52d2e6ea8c80828db0200ea21ff8e290dd09a55
maritere <mari_floyd@hotmail.com>
2025-08-22 11:52:53 -0600

    Se agregaron las clases

---
+++ org/generation/util/banco/CuentaCredito.java
@@ -0,0 +1,11 @@
+package org.generation.util.banco;
+
+import java.util.Date;
+
+public class CuentaCredito {
+
+	public CuentaCredito(int i, String string, Date time, Date time2, float f, double d, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ org/generation/util/banco/CuentaDebito.java
@@ -0,0 +1,11 @@
+package org.generation.util.banco;
+
+import java.util.Date;
+
+public class CuentaDebito {
+
+	public CuentaDebito(int i, String string, Date time, int j, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
